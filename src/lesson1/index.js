c7dd4789bbe56aca45556ae7f88c0ee9faa0bed8
kir4ik <vlados.linkoln@gmail.com>
2020-05-19 02:07:35 +0300

    added lesson 1

--- src/lesson1/index.js
+++ src/lesson1/index.js
@@ -1,5 +1,18 @@
+const calc = {
+  sum: (...args) => args.reduce((acc, n) => acc + n, 0),
+  substract: (...args) => args.reduce((acc, n) => acc - n),
+  devide: (...args) => args.reduce((acc, n) => acc / n),
+  multy: (...args) => args.reduce((acc, n) => acc * n),
+};
+
+
 function lesson1() {
   console.log('lesson1');
+
+  console.log(calc.sum(1, 2, 3) === 6);
+  console.log(calc.substract(12, 2) === 10);
+  console.log(calc.devide(12, 2) === 6);
+  console.log(calc.multy(1, 2, 3, 4) === 24);
 }
 
 export default lesson1;
